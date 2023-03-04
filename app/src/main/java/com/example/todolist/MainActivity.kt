bec31245066261ea96c33efde1b3726584aa71b6
yahyairl <hilariousboy0@gmail.com>
2023-03-04 23:11:09 +0100

    fin du projet normalement: ajout: -Garder le nom la tache enregistrer après avoir modifié l'EditText -Sauvegarder les taches quand j'ajoute une nouvelle tache, quand supprime, quand je check une tache Voilà

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.todolist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.gson.Gson
+import java.util.prefs.Preferences
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -15,28 +19,33 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: TacheAdapter
     private lateinit var addButton: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        data.add(Tache("tache1"))
-        data.add(Tache("tache2"))
-        data.add(Tache("tache3"))
-        data.add(Tache("tache4"))
 
         addButton = findViewById(R.id.addButton)
         recyclerView = findViewById(R.id.recyclerTache)
         recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = TacheAdapter(data,this)
+        data = adapter.restoreTaches(this)
+        adapter = TacheAdapter(data,this)
         recyclerView.adapter = adapter
 
-        addButton.setOnClickListener {
-            data.add(Tache("new tache"))
-            println("bonjour")
+       addButton.setOnClickListener {
+            Tache.MY_VARIABLE++
+            data.add(Tache("Bonjour"+Tache.MY_VARIABLE, false))
             adapter.notifyDataSetChanged()
-            Toast.makeText(this@MainActivity, "Ajout!", Toast.LENGTH_LONG).show()
+            adapter.saveTaches(this, data)
         }
-
-
     }
 
+    override fun onStop() {
+        super.onStop()
+        adapter.saveTaches(this, data)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        adapter.saveTaches(this, data)
+    }
 }
